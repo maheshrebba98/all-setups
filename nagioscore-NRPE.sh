caeddb21ddc798ad34e65b74c3fca4ef83059e28
maheshrebba98 <136562552+maheshrebba98@users.noreply.github.com>
2023-08-17 11:01:00 +0530

    Update nagioscore-NRPE.sh

--- nagioscore-NRPE.sh
+++ nagioscore-NRPE.sh
@@ -5,3 +5,6 @@ sudo -i
 # INSTALL NRPE PLUGIN
 apt install nagios-plugins nagios-nrpe-server -y
 systemctl status nagios-nrpe-server
+# vim /etc/nagios/nrpe.cfg (Enter Nagios Server Public IP #:61)
+  # systemctl restart nagios-nrpe-server
+  # /usr/sbin/nrpe -V /etc/nagios/nrpe.cfg -f
