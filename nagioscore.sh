3f38babdd88226c9a6f4658efda8b7e8e203a6b0
maheshrebba98 <136562552+maheshrebba98@users.noreply.github.com>
2023-08-17 10:50:54 +0530

    Update nagioscore.sh

--- nagioscore.sh
+++ nagioscore.sh
@@ -1,3 +1,4 @@
+#  NAGIOS SERVER
 # UPDATE
 sudo apt update -y
 sudo apt upgrade -y
@@ -22,3 +23,16 @@ a2enmod rewrite cgi
 systemctl restart apache2
 make install-exfoliation
 make install-classicui
+# INSTALL NAGIOS PLUGINS
+VER="2.3.3"
+#TO GET NAGIOS PLUGINS:
+curl -SL https://github.com/nagios-plugins/nagios-plugins/releases/download/release-$VER/nagios-plugins-$VER.tar.gz | tar -xzf -
+cd nagios-plugins-2.3.3/
+./configure
+make install
+#Create a nagiosadmin account for logging into the Nagios web interface. Note the password you need it while login to Nagios web console.
+sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+sudo service apache2 restart
+sudo /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+systemctl enable --now nagios
+systemctl status nagios
