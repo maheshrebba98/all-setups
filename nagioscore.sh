42a841452cf2ca68d605d9605247e282c361a09a
maheshrebba98 <136562552+maheshrebba98@users.noreply.github.com>
2023-08-17 09:59:46 +0530

    Create nagioscore.sh

---
+++ nagioscore.sh
@@ -0,0 +1,31 @@
+# UPDATE
+sudo apt update -y
+sudo apt upgrade -y
+sudo -i
+
+# PRE-REQUESITES
+apt install wget vim unzip curl gcc openssl build-essential libgd-dev libssl-dev libapache2-mod-php php-gd php apache2 -y
+
+#INSTALL NAGIOS CORE:
+export VER="4.4.6"
+curl -SL https://github.com/NagiosEnterprises/nagioscore/releases/download/nagios-$VER/nagios-$VER.tar.gz | tar -xzf -
+cd /root/nagios-4.4.6/
+
+#TO COMPILE:
+
+./configure
+
+make all
+make install-groups-users
+usermod -a -G nagios nagios
+make install
+make install-init
+make install-config
+make install-commandmode
+make install-webconf
+
+a2enmod rewrite cgi
+systemctl restart apache2
+
+make install-exfoliation
+make install-classicui
