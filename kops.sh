74c6fa2d2e5ee535385ad736c6a999b92de46a49
maheshrebba98 <136562552+maheshrebba98@users.noreply.github.com>
2023-09-01 10:49:00 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -16,3 +16,10 @@ aws s3api put-bucket-versioning --bucket rahamssshaik09.k8s.local --region us-ea
 export KOPS_STATE_STORE=s3://rahamssshaik09.k8s.local
 kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name rahams.k8s.local --yes --admin
+
+# List cluster : kops get cluster
+# Edit the Cluster name : kops edit rahams.k8s.local 
+# Edit the node instance-group for no.of worker nodes : kops edit ig --name=rahams.k8s.local nodes-us-east-1a
+# Edit the Master instance-group for no.of Master nodes : kops edit ig --name=rahams.k8s.local master-us-east-1a
+     # after modifed the command in below commands need to update the cluster : kops update cluster --name rahams.k8s.local --yes --admin
+     # Restart the cluster will get other command and run it : kops rolling-update cluster --yes
