6256ae314a639fefc51af4cfc060f448b1bce61b
maheshrebba98 <136562552+maheshrebba98@users.noreply.github.com>
2024-04-15 13:46:41 +0530

    Update Ubuntu-Kops.sh

--- Ubuntu-Kops.sh
+++ Ubuntu-Kops.sh
@@ -19,3 +19,13 @@ aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham007899123.k8s.loca
 export KOPS_STATE_STORE=s3://cloudanddevopsbyraham007899123.k8s.local
 kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name rahams.k8s.local --yes --admin
+### Here, executing script we need execute again commands
+   # cat file name.yml --> export KOPS_STATE_STORE=s3://rahamssshaik09.k8s.local
+   # kops validate cluster --wait 10m
+
+# List cluster : kops get cluster
+# Edit the Cluster name : kops edit rahams.k8s.local 
+# Edit the node instance-group for no.of worker nodes : kops edit ig --name=rahams.k8s.local nodes-us-east-1a
+# Edit the Master instance-group for no.of Master nodes : kops edit ig --name=rahams.k8s.local master-us-east-1a
+     # after modifed the command in below commands need to update the cluster : kops update cluster --name rahams.k8s.local --yes --admin
+     # Restart the cluster will get other command and run it : kops rolling-update cluster --yes
